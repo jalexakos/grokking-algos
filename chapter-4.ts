6604fbeff4a5288ceb2aa2b8247b9dd7064dfb31
Josiah Alexakos <jalexakos@linkedin.com>
2024-01-22 08:25:21 -0800

    Solution for exercise 4.3

--- chapter-4.ts
+++ chapter-4.ts
@@ -28,4 +28,23 @@ function countList(arr:Array<any>): number {
     return 1 + countList(arr.slice(1, arr.length));
 }   
 
-countList([1,2,3,4,5]);
+countList([1,2,3,4,5]); // works
+
+// 4.3
+
+function maxNum(arr: Array<number>): number{
+    if (arr.length === 0){
+        return 0;
+    }
+    
+    let max = arr[0];
+    for (let num in arr){
+        if (arr[num] > max){
+            max = arr[num];
+        }
+    }
+    
+    return max;
+}
+
+maxNum([ 0, 3, 6, 9, 12, 15, 18 ]); // works
