db9b953471447513eb9b1be4fffa092b8ddaf2e0
Josiah Alexakos <jalexakos@linkedin.com>
2024-01-29 08:45:13 -0800

    Solution for exercise 4.4; got a little help from ChatGPT

--- chapter-4.ts
+++ chapter-4.ts
@@ -54,10 +54,17 @@ function binarySearchRecursive(arr: Array<number>, target: number): number {
     const midPoint = Math.floor((arr.length - 1)/2);
     if (arr[midPoint] === target) return midPoint;
     else if (arr.length === 1) return -1;
-    else if (arr[midPoint] > target) return binarySearchRecursive(arr.slice(0, midPoint - 1), target);
-    else return binarySearchRecursive(arr.slice(midPoint + 1), target);
+    else if (arr[midPoint] > target) return binarySearchRecursive(arr.slice(0, midPoint), target);
+    else return (binarySearchRecursive(arr.slice(midPoint + 1), target) + (midPoint + 1));
 }
 
-binarySearchRecursive([ 0, 3, 6, 9, 12, 15, 18 ], 15) // 5
+console.log(binarySearchRecursive([ 0, 3, 6, 9, 12, 15, 18 ], 2)) // -1
+console.log(binarySearchRecursive([ 0, 3, 6, 9, 12, 15, 18 ], 6)) // 2
+console.log(binarySearchRecursive([ 0, 3, 6, 9, 12, 15, 18 ], 15)) // 5
+console.log(binarySearchRecursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 6)); // 5
 
 // this solution doesn't work because we get the wrong index, as we're slicing the array. Thus, we're getting the right index on the subarray, but not the actual array.
+// UPDATE: Got the solution to work by passing the midpoint + 1 to the answer for a right sided slice. However, it still
+// had issues, which I had chatGPT debug for me. I was slicing incorrectly on my left sided slice - I was passing 
+// midpoint - 1 as the second value in slice, when I should pass midpoint, as slice is non-inclusive of the second value
+// (thus, it would not include the midpoint value in the new index).
