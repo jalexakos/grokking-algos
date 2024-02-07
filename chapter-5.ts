efc982e5fd178e4ae330cab7f9ed2e7d8cc9eaf5
Josiah Alexakos <jalexakos@linkedin.com>
2024-02-07 09:24:56 -0800

    My solutions for the first exercises of chapter 5

---
+++ chapter-5.ts
@@ -0,0 +1,21 @@
+// 5.1-5.4: Which of these hash functions are consistent?
+
+// 5.1
+
+// f(x) = 1 
+// consistent
+
+// 5.2
+
+// f(x) = Math.random()
+// inconsistent
+
+// 5.3
+
+// f(x) = nextEmptySlot()
+// inconsistent
+
+// 5.4
+
+// f(x) = len(x)
+// consistent
