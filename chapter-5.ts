69a3c4c967209c1df52b9647e96a3c0501438345
Josiah Alexakos <jalexakos@linkedin.com>
2024-02-07 12:29:06 -0800

    My solutions for the second exercises of chapter 5

--- chapter-5.ts
+++ chapter-5.ts
@@ -19,3 +19,15 @@
 
 // f(x) = len(x)
 // consistent
+
+// 5.5
+
+// in this case, use the letter and prime number mapping for your hash function (D).
+
+// 5.6
+
+// in this case, the hash function that uses the length of the string as the index would be the best (B).
+
+// 5.7
+
+// in this case, use the first character of the string as the index for your hash function (C).
