2cb68757497d6332b06bcefa848c22df08270257
Josiah Alexakos <jalexakos@linkedin.com>
2024-02-27 11:24:39 -0800

    My solutions for the second set of exercises of chapter 6

--- chapter-6.ts
+++ chapter-6.ts
@@ -7,7 +7,17 @@
 // cab to the left to cat, then right to bat.
 
 // 6.3
+// Based on the dependency structure, A and C are invalid lists. B is a valid list.
 
 // 6.4
+// A possible valid list:
+// 1. wake up
+// 2. exercise
+// 3. shower
+// 4. brush teeth
+// 5. get dressed
+// 6. eat breakfast
+// 7. pack lunch
 
 // 6.5
+// A and C are also trees. B is not, as it has an edge which points back to the first node.
