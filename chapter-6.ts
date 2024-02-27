39811b6dd1079c69865d4fa1cb3e8e2587b9dde7
Josiah Alexakos <jalexakos@linkedin.com>
2024-02-27 11:10:09 -0800

    My solutions for the first set of exercises of chapter 6

---
+++ chapter-6.ts
@@ -0,0 +1,13 @@
+// 6.1
+// the length of the shortest path is two steps.
+// s to the left, then s to the left again, reaches F.
+
+// 6.2
+// the length of the shortest path is two steps.
+// cab to the left to cat, then right to bat.
+
+// 6.3
+
+// 6.4
+
+// 6.5
