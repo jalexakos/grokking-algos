5c4d1f315611a7b9a47bce3df3d146f8d9b38c52
Josiah Alexakos <jalexakos@linkedin.com>
2024-04-11 10:52:27 -0700

    First set of questions for chapter 9

---
+++ chapter-9.ts
@@ -0,0 +1,5 @@
+// 9.1
+// No, you should not steal the MP3 player, as it will not increase the greatest 
+// 1lb value (since it's worth only $1000, compared to the current 1lb max of $2000).
+// Wrong. With the MP3, you can steal it, the iPhone, and the guitar, all each 1lb, for 
+// a total of $3lbs and $4500.
