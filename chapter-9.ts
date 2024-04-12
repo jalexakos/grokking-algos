4faf4ca7dc9701ea6edbd963f9a1f71664ceaa5b
Josiah Alexakos <jalexakos@linkedin.com>
2024-04-11 17:23:34 -0700

    Second set of questions for chapter 9

--- chapter-9.ts
+++ chapter-9.ts
@@ -3,3 +3,11 @@
 // 1lb value (since it's worth only $1000, compared to the current 1lb max of $2000).
 // Wrong. With the MP3, you can steal it, the iPhone, and the guitar, all each 1lb, for 
 // a total of $3lbs and $4500.
+
+// 9.2
+// The optimal set is Water + Food + Camera (total 25).
+// Correct.
+
+// 9.3
+// The longest common substring will be "lue", of length 3.
+// Correct.
